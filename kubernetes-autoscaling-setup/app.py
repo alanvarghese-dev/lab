55f4524cb598fdf05a20e532ebb30d24d6624300
Alan Varghese <thealanvarghese@gmail.com>
2026-09-21 08:14:39 +0000

    Initial commit for kubernetes autoscaling app project

---
+++ kubernetes-autoscaling-setup/app.py
@@ -0,0 +1,21 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == "/health":
+            self.send_response(200)
+            self.end_headers()
+            self.wfile.write(b"OK")
+            return
+
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b"Kubernetes Autoscaling App")
+
+    def log_message(self, format, *args):
+        return
+
+
+server = HTTPServer(("0.0.0.0", 8000), Handler)
+server.serve_forever()
